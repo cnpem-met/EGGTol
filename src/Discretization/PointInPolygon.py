f56871bdd4b619f30e7371537edacf20907a1c04
Hideak <willianhideak@hotmail.com>
2017-08-15 18:14:31 -0300

    Correcting Identation in PointInPolygon.py

--- src/Discretization/PointInPolygon.py
+++ src/Discretization/PointInPolygon.py
@@ -15,15 +15,15 @@ def pointInPolygon(x, y, poly):
     # Returns: * Boolean result = True if the point lies inside, and False otherwise.
     """
 
-        num = len(poly)
-        i = 0
-        j = num - 1
-        result = False
-        # Checking the 'insideness' for each given points:
-        for i in range(num):
-                if  ((poly[i][1] > y) != (poly[j][1] > y)) and \
-                        (x < (poly[j][0] - poly[i][0]) * (y - poly[i][1]) / \
-                        (poly[j][1] - poly[i][1]) + poly[i][0]):
-                    result = not result
-                j = i
-        return result
+    num = len(poly)
+    i = 0
+    j = num - 1
+    result = False
+    # Checking the 'insideness' for each given points:
+    for i in range(num):
+            if  ((poly[i][1] > y) != (poly[j][1] > y)) and \
+                    (x < (poly[j][0] - poly[i][0]) * (y - poly[i][1]) / \
+                    (poly[j][1] - poly[i][1]) + poly[i][0]):
+                result = not result
+            j = i
+    return result
