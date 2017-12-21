1893e10ba978630dc64639b6d1ef5f93775a09be
Hideak <willianhideak@hotmail.com>
2017-12-21 16:13:12 -0200

    Changing version to v0.7.107

--- src/main.py
+++ src/main.py
@@ -56,7 +56,7 @@ class MainWindow(QMainWindow):
 
         # Defining Main Window Properties:
         self.light = True
-        self.title = 'Gerador de Nuvem de Pontos v0.7.105'
+        self.title = 'Gerador de Nuvem de Pontos v0.7.107'
         self.setWindowTitle(self.title)
         self.setWindowIcon(QIcon('..\\icons\\desktopIcons\\main.png'))
         self.resize(1300, 650)
