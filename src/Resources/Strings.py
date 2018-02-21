e4894b4c454fc7ac4692dea6c4fcb95d4b9955fe
hideak <willianhideak@hotmail.com>
2018-02-21 15:39:48 -0300

    Added a strings resource file

---
+++ src/Resources/Strings.py
@@ -0,0 +1,13 @@
+"""
+# Module: Strings.py
+# Descriptin: This module acts as a string resource. Other Python files can import all the
+# strings in this module to further use.
+"""
+
+class MyStrings():
+    """
+    # Class: MyStrings
+    # Description: This class contains strings for further use. Each string is stored as a
+    class attribute, also known as a static attribute.
+    """
+    pass
