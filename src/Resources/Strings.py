6f4aa329822f1060a75ff1d904ef9d2df292cc9a
hideak <willianhideak@hotmail.com>
2018-04-10 20:21:36 -0300

    Updating to version v.0.7.144

--- src/Resources/Strings.py
+++ src/Resources/Strings.py
@@ -13,7 +13,7 @@ class MyStrings():
 
     # Window Title Strings:
     applicationTitle = 'Point Cloud Generator'
-    applicationVersion = 'v0.7.141'
+    applicationVersion = 'v0.7.144'
 
     # MenuBar Strings:
     menuBarFile = 'File'
